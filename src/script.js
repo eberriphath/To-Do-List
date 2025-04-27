b8303c5e24a3818af88eb5b70a06ad0a609768fe
eberriphath <riphatheber@gmail.com>
2025-04-27 23:38:12 +0300

    update address

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-
+    const contactsArray = [];
     const form = document.getElementById('todo-form');
     const taskInput = document.getElementById('task');
     const todoList = document.getElementById('todo-list');
@@ -15,4 +15,34 @@ document.addEventListener('DOMContentLoaded', function() {
             taskInput.value = '';
         }
     });
+    
+
+    const addressBookForm = document.getElementById('address-book-form');
+    const contactsList = document.getElementById('contacts-list');
+
+    addressBookForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const phone = document.getElementById('phone').value;
+        
+        const contact = {
+            name: name,
+            email: email,
+            phone: phone
+        };
+
+        contactsArray.push(contact);
+        const contactItem = document.createElement('li');
+        contactItem.classList.add('contact-item');
+        contactItem.innerHTML = `
+            <strong>Name:</strong> ${contact.name} <br>
+            <strong>Email:</strong> ${contact.email} <br>
+            <strong>Phone:</strong> ${contact.phone}
+        `;
+        contactsList.appendChild(contactItem);
+        addressBookForm.reset();
+        console.log("Contacts List: ", contactsArray);
+    });
 });
+
